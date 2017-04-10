7215bfa3a77f781152250899e6b7559b85cae233
fanxiaobin <fanxiaobin@liweideMacBook-Air.local>
2017-04-10 18:07:03 +0800

    first init commit

--- FF_CarouselViewLayout/ViewController.m
+++ FF_CarouselViewLayout/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "HJCarouselViewLayout.h"
 
-@interface ViewController ()
+@interface ViewController ()<UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout>
+
+@property (nonatomic,strong) UICollectionView *collectionView;
 
 @end
 
@@ -17,6 +20,43 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    HJCarouselViewLayout *layout = [[HJCarouselViewLayout alloc] initWithAnim:HJCarouselAnimLinear];
+    layout.itemSize = CGSizeMake(self.view.bounds.size.width - 60, self.view.bounds.size.height - 120);
+    layout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+    //layout.visibleCount = 3;
+    self.collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:layout];
+    self.collectionView.backgroundColor = [UIColor whiteColor];
+    self.collectionView.delegate = self;
+    self.collectionView.dataSource = self;
+    
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"UICollectionViewCell"];
+    
+    [self.view addSubview:self.collectionView];
+    
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    return 6;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"UICollectionViewCell" forIndexPath:indexPath];
+    
+    cell.clipsToBounds = YES;
+    cell.layer.cornerRadius = 6.0;
+    
+    CGFloat r = arc4random() % 255 / 255.0;
+    CGFloat g = arc4random() % 255 / 255.0;
+    CGFloat b = arc4random() % 255 / 255.0;
+    
+    UIColor *rColor = [UIColor colorWithRed:r green:g blue:b alpha:1.0];
+    
+    cell.contentView.backgroundColor = rColor;
+    
+    return cell;
 }
 
 
